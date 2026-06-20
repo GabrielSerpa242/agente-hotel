7a3544126d707a659085109009be039f961afe1b
GabrielSerpa242 <gabriel.serpa2010@hotmail.com>
2026-06-20 14:20:02 -0300

    testando

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-#Importando as bibliotecas
-from flask import Flask,jsonify,request
+#importando as bibliotecas
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from agno.models.openai import OpenAIChat
 from agno.agent import Agent
@@ -7,33 +7,29 @@ from dotenv import load_dotenv
 
 #Leitura da chave de API
 load_dotenv()
-#Criar o nosso app
-app = Flask (__name__)
-#Habilitar o cors
+#Criando a aplicação Flask
+app = Flask(__name__)
+#Habilitar o cors para permitir requisições de outros domínios
 CORS(app)
-
-#Criar o agente
-agente = Agent (
-    model=OpenAIChat(id="gpt-4o-mini"),
-    description="Você é um agente virtual do Hotel Travesseiro Nervoso, slogan: Aqui até a insônia dorme"
-    "Você responde de forma clara e humorada, informações sobre quartos,serviços, reservas e preços"
-    "Quarto Standard ($500), Quarto Deluxe ($700), Quarto Suíte Presidencial ($1000)",
-    markdown=True
+#Criar agente
+agente = Agent(
+    model = OpenAIChat(id="gpt-4o-mini"),
+    description = "Voce é um agente virtual do hotel Travesseiro Nervoso",
+    instructions = "Voce reponde de forma clara e humorada, informações sobre quartos, serviços, reservas e outras dúvidas relacionadas ao hotel Travesseiro Nervoso",
+    tools = ["Quarto Standbard(R$500)", "Quarto Deluxe(R$700)", "Quarto Suite Presidecial(R$1000)"],
+    markdown = True
 )
 
-#Criar a rota VAZIA e o método GET
 @app.route("/", methods=['GET'])
 def testar():
-    return jsonify({"mensagem":"API funcionando"})
+    return jsonify({"message": "Bem-vindo ao agente virtual do hotel Travesseiro Nervoso!"})
 
-#Criar a rota e o método POST
-@app.route("/chat",methods=['POST'])
+@app.route("/chat", methods=['POST'])
 def pergunta():
     dados = request.get_json()
-    pergunta = dados['pergunta']
+    pergunta = dados["pergunta"]
     resposta = agente.run(pergunta)
-    return jsonify({"resposta":resposta.content})
+    return jsonify({"resposta": resposta.content})
 
-#Rodar o nosso app
-if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=8000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
